d6df0bf5297c55f6ebd3a679a56399fa759a3467
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-17 16:33:25 +0300

    Add the deleteTask controller implementation logic

---
+++ src/controllers/deleteTask.controller.ts
@@ -0,0 +1,16 @@
+import { Response, Request } from "express";
+import { role } from "../middlewares/TasksUtils";
+
+export async function deleteTask(req: Request, res: Response){
+    const id = req.params.id;
+    try{
+        const deleted = await role.tasks.delete({
+            where:{id}
+        })
+        if(deleted) res.status(201).json({message: `Successfully deleted: Task with id ${id}`});
+        else res.status(400).json({message: `Error deleting task id: '${id}'`})
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: "Internal Server Error !! :("});
+    }
+}
