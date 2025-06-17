175bd6d17e83f61b35108ceda469cc85f0f525df
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-17 23:28:44 +0300

    Rename getSingleTask to getOneTask

--- src/controllers/controllers.exports.ts
+++ src/controllers/controllers.exports.ts
@@ -1,5 +1,5 @@
 import { getTasks } from "./getTasks.controller";
-import { getSingleTask } from "./getASingleTask.controller";
+import { getSingleTask } from "./getOneTask.controller";
 import { deleteTask } from "./deleteTask.controller";
 import { updateTask } from "./updateTask.controller";
 import { createTask } from "./createTask.controller";
