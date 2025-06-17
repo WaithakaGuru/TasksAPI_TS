68faab54a1382dbe8640df2c2dbb32538609fa2b
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-17 16:42:41 +0300

    Add controller.exports.ts to bundled up all controller exports into a single export

---
+++ src/controllers/controllers.exports.ts
@@ -0,0 +1,7 @@
+import { getTasks } from "./getTasks.controller";
+import { getSingleTask } from "./getSingletask.controller";
+import { deleteTask } from "./deleteTask.controller";
+import { updateTask } from "./updateTask.controller";
+import { createTask } from "./createTask.controller";
+
+export default {getSingleTask, getTasks, createTask, deleteTask, updateTask}
