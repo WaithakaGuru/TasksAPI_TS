79c732aca71b49158bf0aeef8c9dc6d89af7131f
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-17 16:05:38 +0300

    Add the updateTask Controller

--- src/controllers/updateTask.controller.ts
+++ src/controllers/updateTask.controller.ts
@@ -2,5 +2,16 @@ import { Request, Response } from "express";
 import { role } from "../middlewares/TasksUtils";
 
 export async function updateTask(req: Request, res: Response){
-
+    const {id, title, taskInfo} = req.body
+    try{
+        const updatedTask = await role.tasks.update({
+            data: {id, title, taskInfo},
+            where:{id}
+        })
+        if(updatedTask) res.send(`Task id: ${id}`);
+        else res.status(400).json({message: `Failed: Create to update task with id: ${id} !! :(`});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: "Internal Server Error !! :("});
+    }
 }
