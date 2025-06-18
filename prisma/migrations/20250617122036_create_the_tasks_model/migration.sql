5ae2ee316a395cdb93f7ac34614bc12f688da8c2
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-18 10:56:17 +0300

    migration fix

--- prisma/migrations/20250617122036_create_the_tasks_model/migration.sql
+++ prisma/migrations/20250617122036_create_the_tasks_model/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "tasks" (
     "id" TEXT NOT NULL,
     "Task_Title" TEXT NOT NULL,
-    "Taksk_Description" TEXT NOT NULL,
+    "Task_Description" TEXT NOT NULL,
     "Task_Is_Complete" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
