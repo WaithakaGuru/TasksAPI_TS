032a086560208a549acd0e9d81c758d126f9aa54
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-18 07:34:01 +0300

    Save devDepndencies as required and Put the port number as an environment Variable

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { app, config} from "./middlewares/TasksUtils.js";
 import router from "./routers/tasks.router.js";
 config();
 
-const port = process.env.PORT || 4330;
+const port = process.env.PORT || process.env.SERVER_PORT_NUMBER;
 
 app.use("/", router);
 app.listen(port, () => {
